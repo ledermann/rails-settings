17ab9fde8692b7664b48d0a7a0bb35480c4c65b0
Georg Ledermann <mail@georg-ledermann.de>
2013-02-28 08:24:17 +0100

    Scopes: Always use lambda

--- lib/rails-settings/scopes.rb
+++ lib/rails-settings/scopes.rb
@@ -2,18 +2,20 @@ module RailsSettings
   module Scopes
     def self.included(base)
       base.class_eval do
-        scope :with_settings,
+        scope :with_settings, lambda {
           joins(:setting_object)
+        }
 
         scope :with_settings_for, lambda { |key|
           joins(:setting_object).
           where("settings.value LIKE '%#{key}: %'")
         }
 
-        scope :without_settings, 
+        scope :without_settings, lambda {
           joins("LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
                                         settings.target_type = '#{self.base_class.name}')").
           where('settings.id IS NULL')
+        }
 
         scope :without_settings_for, lambda { |key|
           joins("LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
