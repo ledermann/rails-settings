030935322105d7075ee1e227bbce80383903c63c
Georg Ledermann <mail@georg-ledermann.de>
2014-12-21 10:06:03 +0100

    Rails 4.2 compatibility
    In migrations, using #timestamps without options is deprecated

--- lib/generators/rails_settings/migration/templates/migration.rb
+++ lib/generators/rails_settings/migration/templates/migration.rb
@@ -4,7 +4,7 @@ class RailsSettingsMigration < ActiveRecord::Migration
       t.string     :var,    :null => false
       t.text       :value
       t.references :target, :null => false, :polymorphic => true
-      t.timestamps
+      t.timestamps :null => true
     end
     add_index :settings, [ :target_type, :target_id, :var ], :unique => true
   end
