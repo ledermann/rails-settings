cfd9e6c14f15ea2a8c650c3202ceb451fa0747cc
Georg Ledermann <mail@georg-ledermann.de>
2016-05-26 08:15:20 +0200

    Rails 5: Use versioned migration

--- lib/generators/rails_settings/migration/templates/migration.rb
+++ lib/generators/rails_settings/migration/templates/migration.rb
@@ -1,4 +1,10 @@
-class RailsSettingsMigration < ActiveRecord::Migration
+MIGRATION_BASE_CLASS = if ActiveRecord::VERSION::MAJOR >= 5
+  ActiveRecord::Migration[5.0]
+else
+  ActiveRecord::Migration
+end
+
+class RailsSettingsMigration < MIGRATION_BASE_CLASS
   def self.up
     create_table :settings do |t|
       t.string     :var,    :null => false
