88b2a54e881403cefdb6a4ee3ee7dd399b39e2e0
farkmarnum <mark@nayya.com>
2022-02-28 16:04:03 -0500

    Add tests for single character settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,7 +45,7 @@ end
 
 class User < ActiveRecord::Base
   has_settings do |s|
-    s.key :dashboard, :defaults => { :theme => 'blue', :view => 'monthly', :filter => true, owner_name: -> (target) { target.name } }
+    s.key :dashboard, :defaults => { :theme => 'blue', :view => 'monthly', :a => 'b', :filter => true, owner_name: -> (target) { target.name } }
     s.key :calendar,  :defaults => { :scope => 'company'}
   end
 end
