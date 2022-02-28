43b9b31d85a4d32bbae980a3553449b34e1f517c
farkmarnum <mark@nayya.com>
2022-02-28 17:15:35 -0500

    Add another test for Hashes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,7 +46,7 @@ end
 class User < ActiveRecord::Base
   has_settings do |s|
     s.key :dashboard, :defaults => { :theme => 'blue', :view => 'monthly', :filter => true, owner_name: -> (target) { target.name } }
-    s.key :calendar,  :defaults => { :scope => 'company', :events => [] }
+    s.key :calendar,  :defaults => { :scope => 'company', :events => [], :profile => {} }
   end
 end
 
