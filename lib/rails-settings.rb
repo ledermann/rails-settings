8a18af880eba89243fcc9b1fefd0d466ef69ceb1
Shashank Dass <shashank.dass@coupa.com>
2025-08-08 22:42:41 -0700

    Use ActiveSupport.on_load for initialization (#118)
    Defer the inclusion until after ActiveRecord has been fully loaded, improving booting performance and following Rails best practices (#118)

--- lib/rails-settings.rb
+++ lib/rails-settings.rb
@@ -3,11 +3,13 @@ require 'rails-settings/configuration'
 require 'rails-settings/base'
 require 'rails-settings/scopes'
 
-ActiveRecord::Base.class_eval do
-  def self.has_settings(*args, &block)
-    RailsSettings::Configuration.new(*args.unshift(self), &block)
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.class_eval do
+    def self.has_settings(*args, &block)
+      RailsSettings::Configuration.new(*args.unshift(self), &block)
 
-    include RailsSettings::Base
-    extend RailsSettings::Scopes
+      include RailsSettings::Base
+      extend RailsSettings::Scopes
+    end
   end
 end
